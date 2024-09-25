08abfe7bc737c29b81b493ec126bf49d0e031ddd
strahd <jv.palha@gmail.com>
2024-09-25 17:26:31 -0300

    refactore: change core entities

--- core/entities/deck-list.ts
+++ core/entities/deck-list.ts
@@ -5,11 +5,17 @@ export class DeckList {
     public _id: string
     public player_id: string
     public deck_name: string
+    public main_deck: Card[]
+    public side_deck: Card[]
+    public extra_deck: Card[]
 
-    constructor({player_id, deck_name}: DeckList){
+    constructor({player_id, deck_name, main_deck, extra_deck, side_deck}: DeckList){
         this._id = randomUUID()
         this.player_id = player_id
         this.deck_name = deck_name
+        this.main_deck = main_deck
+        this.side_deck = side_deck
+        this.extra_deck = extra_deck
     }
 
     
