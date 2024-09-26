13b4b5a3ade5b9bbd7dc856034696d5a6c9cef88
strahd <jv.palha@gmail.com>
2024-09-26 12:16:40 -0300

    add: use case to a player register himself to a tournament and test

---
+++ core/use-cases/register-to-tournament-use-case.spec.ts
@@ -0,0 +1,36 @@
+import {beforeEach, describe, expect, it} from "vitest";
+import { InMemoryTournamentRepository } from "../repositories/in-memory/in-memory-tournament-repositoy";
+import { Player, PlayerProps } from "../entities/player";
+import { createTournamentHelper } from "./helpers/create-tournament-helper";
+import { createDeckListHelper } from "./helpers/create-deck-list-helper";
+import { InMemoryDeckListRepository } from "../repositories/in-memory/in-memory-deck-list-repository";
+import { RegisterToTournamentUseCase } from "./register-to-tournament-use-case";
+
+describe("register to an tournament use case", () => {
+    let sut: RegisterToTournamentUseCase
+    let tournamentRepository: InMemoryTournamentRepository
+    let deckListRepository: InMemoryDeckListRepository
+
+    beforeEach(()=>{
+        tournamentRepository = new InMemoryTournamentRepository()
+        deckListRepository = new InMemoryDeckListRepository()
+        sut = new RegisterToTournamentUseCase(tournamentRepository)
+
+    })
+
+    it("should be able to a player register to an tournament", async () => {
+        const {tournament} = await createTournamentHelper(tournamentRepository)
+        const {deck_list} = await createDeckListHelper(deckListRepository)
+        const playerInformation: PlayerProps = {
+            name: "Player 1",
+            deck_list: deck_list
+        }
+        const player = new Player(playerInformation)
+
+        const {
+            message
+        } = await sut.execute({player, tournament_id: tournament._id})
+
+        expect(message).toBe("Player registered to tournament")
+    })
+})
---
+++ core/use-cases/register-to-tournament-use-case.ts
@@ -0,0 +1,31 @@
+import { Player } from "../entities/player";
+import { TournamentRepository } from "../repositories/tournament-repository";
+import { TournamentNotFoundError } from "./errors/tournament-not-found-error";
+
+interface RegisterToTournamentUseCaseRequest {
+    player: Player
+    tournament_id: string
+}
+type RegisterToTournamentUseCaseResponse = {
+    message: string
+}
+
+export class RegisterToTournamentUseCase{
+    constructor(
+        private tournamentRepository: TournamentRepository
+    ){}
+
+    async execute({player, tournament_id}: RegisterToTournamentUseCaseRequest): Promise<RegisterToTournamentUseCaseResponse>{
+        const tournament = await this.tournamentRepository.findById(tournament_id)
+
+        if(!tournament){
+            throw new TournamentNotFoundError()
+        }
+
+        await this.tournamentRepository.addPlayer(tournament_id, player)
+        
+        return {
+            message: "Player registered to tournament"
+        }
+    }
+}
