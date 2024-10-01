c6a982de71803a562d8d9c23ee5841801537e33e
strahd <jv.palha@gmail.com>
2024-10-01 15:54:04 -0300

    fix: change interfaces to abstract classes

--- src/core/repositories/tournament-repository.ts
+++ src/core/repositories/tournament-repository.ts
@@ -1,10 +1,10 @@
 import { Player } from "../entities/player";
 import { Tournament, TournamentProps } from "../entities/tournament";
 
-export interface TournamentRepository {
-    create: (tournament: TournamentProps) => Promise<Tournament>
-    findById: (id: string) => Promise<Tournament | undefined>
-    addPlayer: (tournament_id: string, player: Player) => Promise<Tournament>
-    fetchAll: (open: boolean) => Promise<Tournament[]>
-    close: (id: string, key: string) => Promise<Tournament>
+export abstract class TournamentRepository {
+    abstract create: (tournament: TournamentProps) => Promise<Tournament>
+    abstract findById: (id: string) => Promise<Tournament | undefined>
+    abstract addPlayer: (tournament_id: string, player: Player) => Promise<Tournament>
+    abstract fetchAll: (open: boolean) => Promise<Tournament[]>
+    abstract close: (id: string, key: string) => Promise<Tournament>
 }
