79966c7fd4515b0b43f4087e268fe49240f31562
strahd <jv.palha@gmail.com>
2024-10-01 15:57:50 -0300

    add: providers to database to get repositories

--- src/http/database/mongo/mongo.module.ts
+++ src/http/database/mongo/mongo.module.ts
@@ -4,6 +4,10 @@ import { env } from "src/http/env/config.env";
 import { Player, PlayerSchema } from "./schemas/player.schema";
 import { Tournament, TournamentSchema } from "./schemas/tournament.schema";
 import { DeckList, DeckListSchema } from "./schemas/deck-list.schema";
+import { DeckListRepository } from "src/core/repositories/deck-list-repository";
+import { MongoDeckListRepository } from "./repositories/mongo-deck-list.repository";
+import { TournamentRepository } from "src/core/repositories/tournament-repository";
+import { MongoTournamentRepository } from "./repositories/mongo-tournament.repository";
 
 @Module({
     imports: [
@@ -13,6 +17,16 @@ import { DeckList, DeckListSchema } from "./schemas/deck-list.schema";
             { name: Tournament.name, schema: TournamentSchema },
             { name: DeckList.name, schema: DeckListSchema }
         ])
+    ],
+    providers: [
+        {
+            provide: DeckListRepository,
+            useClass: MongoDeckListRepository
+        },
+        {
+            provide: TournamentRepository,
+            useClass: MongoTournamentRepository
+        }
     ]
 })
 export class MongoModule {}
